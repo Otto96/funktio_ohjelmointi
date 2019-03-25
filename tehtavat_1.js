8afed3273a5cda94365518ff1be078cfa4a71e94
Otto <otto.astrom@metropolia.fi>
2019-03-25 07:27:48 +0000

    tehtävät 1-4

--- tehtavat_1.js
+++ tehtavat_1.js
@@ -1,2 +1,70 @@
+// V1
+// TEHT 1
 
-//ayy lmaooo
+function onPalindromi(sana) {
+    var len = sana.length;
+    if (len < 2)
+        return true;
+    else if (sana[0] != sana[len-1])
+        return false;
+    else 
+        return onPalindromi(sana.slice(1, len-1))
+}
+
+// console.log("saippuakauppias ", onPalindromi("saippuakauppias")) // returns true
+// console.log("abc ", onPalindromi("abc")) // returns false
+
+
+// TEHT 2
+
+function syt(p, q) {
+    if (q == 0)
+        return p;
+    else 
+        return syt(q, p%q);
+}
+
+// console.log(syt(102, 68)); // returns 34
+// console.log(syt(132,222)); // returns 6
+
+
+// TEHT 3
+
+function kjl(p, q) {
+    if (q == 0 && p != 1)
+        return false;
+    else if (p == 1)
+        return true;
+    else 
+        return kjl(q, p%q);
+}
+
+// console.log(kjl(102,68)); // returns false
+// console.log(kjl(35,18)); // returns true
+
+// TEHT 4
+
+function powah(luku, ap, potenssi, x) {
+    if (potenssi <= x) {
+        return luku;
+    } else {
+        return powah(luku*ap, ap, potenssi, x+1)
+    }
+}
+
+// console.log(powah(2,2,2,1)); // 2^2=4
+// console.log(powah(2,2,10,1)); // 2^10=1024
+// console.log(powah(3,3,3,1)); // 3^3=27
+
+// TEHT 5
+
+function käännä(lista, y){
+    var lkm = lista.length;
+    if (lkm <= y) {
+        return lista;
+    } else {
+        return 
+    }
+}
+
+// console.log(käännä([1,2,3], 1));
---
+++ tehtävät_12.js
@@ -0,0 +1,14 @@
+// TEHT 1
+
+function vertaa(x, y) {
+    function(x, y) {
+        if (x > y)
+            return 1;
+        else if (x < y)
+            return -1;
+        else
+            return 0;
+    }
+}
+
+console.log(vertaa(1,2));
