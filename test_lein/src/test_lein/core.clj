fb29b2a84ca0291609fa33bb61551b32ca3a3f20
Otto Åström <ozzy.astrom@gmail.com>
2019-05-13 10:17:04 +0300

    5.2 tehtävät 1-5

--- test_lein/src/test_lein/core.clj
+++ test_lein/src/test_lein/core.clj
@@ -1,7 +1,41 @@
 (ns test-lein.core
   (:gen-class))
 
+(defn isEven []
+  (println "Syötä numero: ")
+  (def number (read-string (read-line)))
+  (cond
+    (< number 0) (when true
+                   (println "Elä anna negatiivista lukua!")
+                   (recur))
+    (even? number) (println "Tasaluku.")
+    :else (println "Pariton luku.")))
+
+(defn printDivisibleBy3 [upperLimit]
+  (doseq [i (range 1 (+ upperLimit 1)) :when (zero? (mod i 3))]
+    (println i)))
+
+(defn lottery []
+  (set (take 7 (set (take 14 (repeatedly #(inc (rand-int 39))))))))
+
+(defn syt [p,q]
+  (cond (zero? q) p
+        :else (recur q (mod p q))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  (println "Tehtävät 1&2: ")
+  (isEven)
+
+  (println)
+  (println "Tehtävä 3: ")
+  (printDivisibleBy3 20)
+
+  (println)
+  (println "Tehtävä 4: ")
+  (println(lottery))
+
+  (println)
+  (println "Tehtävä 5: ")
+  (println(syt 102 68)))
